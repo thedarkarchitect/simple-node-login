0b5f5370012f04bf25c39a3078edaff17b6ba644
thedarkarchitect <romanreynolds240@gmail.com>
2024-03-09 13:35:27 +0300

    delete node modules folder and server.js update for login permission

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 4000;
 const hardcodedPassword = 'Ineedajob'; // Replace with your actual password
+const isLogined = false;
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
@@ -31,6 +32,7 @@ app.post('/login', (req, res) => {
 
   if (enteredPassword === hardcodedPassword) {
     // Password matches, redirect to /course
+    isLogined = true;
     res.redirect('/course');
   } else {
     // Password doesn't match, send an error message
@@ -39,14 +41,18 @@ app.post('/login', (req, res) => {
 });
 
 // Route for "/course" (serves node-course.html, but requires authentication)
-app.get('/course', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Public', 'node-course.html'));
+app.get('/node-course', (req, res) => {
+  if (isLogined) {
+    res.sendFile(path.join(__dirname, 'Public', 'node-course.html'));
+} else {
+  res.redirect('/');
+}
 });
 
 // Redirect to /login if a user tries to access /course without authentication
-app.get('/course', (req, res) => {
-  res.redirect('/login');
-});
+// app.get('/node-course', (req, res) => {
+//   res.redirect('/login');
+// });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
